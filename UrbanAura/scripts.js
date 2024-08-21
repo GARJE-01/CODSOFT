a719131e030879a47959034e0e0de8bc5b3fc8ad
GARJE-01 <t220247@famt.ac.in>
2024-08-21 23:47:40 +0530

    Delete UrbanAura directory

--- UrbanAura/scripts.js
+++
@@ -1,8 +0,0 @@
-// scripts.js
-
-// Example: Add functionality to the Buy Now buttons
-document.querySelectorAll('.product-card .btn').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Product added to cart!');
-    });
-});
